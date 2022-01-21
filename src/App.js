6c3d6f0692951fe897ceff62be5f7e1d090d8a7f
=mplecapt <=mplecapt@mtu.edu>
2022-01-21 10:17:54 -0500

    Fixed routing issues and removed some unused files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Navigate, Routes, Route, useLocation } from "react-router-dom";
+import { Navigate, Routes, Route, useLocation, Link } from "react-router-dom";
 import Statblock from './components/Statblock';
 import CreatureForm, { ShareLink } from './components/Editing';
 import Modal from './components/Modal';
@@ -10,14 +10,12 @@ function App() {
 
 	return (
 		<div>
-			<Routes location={state?.backgroundLocation || location}>
-				<Route path="/kaltergrat">
-					<Route index element={<Navigate to='/kaltergrat/create' />} />
-					<Route path="create" element={<CreatureForm />} />
-					<Route path="edit/:data" element={<CreatureForm />} />
-					<Route path="view/:data" element={<Statblock />} />
-				</Route>
-				<Route path="*" element={<h1>:404</h1>} />
+			<Routes location={state?.backgroundLocation || location}>			
+				<Route index element={<Navigate to='/create' />} />
+				<Route path="create" element={<CreatureForm />} />
+				<Route path="edit/:data" element={<CreatureForm />} />
+				<Route path="view/:data" element={<Statblock />} />
+				<Route path="*" element={<div><h1>:404</h1><Link to='/create'>Home</Link></div>} />
 			</Routes>
 			{state?.backgroundLocation && (
 				<Routes>
